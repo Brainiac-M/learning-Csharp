d5a9f8ab343c587bb05fe9630a35965dc2814396
fox_coder508 <mahmood.ademoye16@gmail.com>
2024-06-23 23:03:17 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,230 @@
+﻿// string val1 = "a";
+// string val2 = "A";
+// Console.WriteLine(val1.Trim().ToLower() == val2.Trim().ToLower());
+
+/*For loop*/
+// string [] names = {"Bob", "Malik", "Saheed"};
+
+// for (int i = names.Length - 1; i >= 0; i--){
+//     Console.WriteLine(names[i]);
+// }
+
+
+/*======================================================*/
+/*======================================================*/
+/*======================================================*/
+/*FIZZBUZZ GAME*/
+// for(int i = 1; i <= 100; i++){
+//     if((i % 3 == 0) && (i % 5 == 0)){
+//         Console.WriteLine($"{i} - FizzBuzz");
+//     }
+//     else if(i % 3 == 0){
+//         Console.WriteLine($"{i} - Fizz");
+//     }else if(i % 5 == 0){
+//         Console.WriteLine($"{i} - Buzz");
+//     }
+//     else{
+//         Console.WriteLine(i);
+//     }
+// }
+
+/*======================================================*/
+/*======================================================*/
+/*======================================================*/
+/*DO-WHILE LOOP*/
+
+// Random random = new Random();
+// int current = 0;
+
+// do{
+//     current = random.Next(1,20);
+//     Console.WriteLine(current);
+// }while(current != 9);
+
+
+
+// Random random = new Random();
+// int current = random.Next(1,20);
+
+// do{
+//     current = random.Next(1,20);
+
+//     if(current >=  8) continue;
+
+//     Console.WriteLine(current);
+// }while(current != 10);
+
+
+/*======================================================*/
+/*======================================================*/
+/*======================================================*/
+/*WHILE LOOP*/
+// Random random = new Random();
+// int current = random.Next(1,20);
+// while(current >= 5){
+//     Console.WriteLine(current);
+//     current = random.Next(1,20);
+// }
+// Console.WriteLine($"Last number: {current}");
+
+
+/*======================================================*/
+/*======================================================*/
+/*======================================================*/
+/*ROLE PLAYING GAME*/
+// int hero = 10;
+// int monster = 10;
+
+// Random dice = new Random();
+
+// do
+// {
+//     int roll = dice.Next(1, 11);
+//     monster -= roll;
+//     Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
+
+//     if (monster <= 0) continue;
+
+//     roll = dice.Next(1, 11);
+//     hero -= roll;
+//     Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
+
+// } while (hero > 0 && monster > 0);
+
+// Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
+
+
+/*======================================================*/
+/*======================================================*/
+/*======================================================*/
+/*VALIDATE INTEGER INPUT*/
+
+// string? readResult;
+// string valueEntered = "";
+// int numValue = 0;
+// bool validNubmer = false;
+
+// Console.WriteLine("Enter an integer value between 5 and 10");
+
+// do{
+//     readResult = Console.ReadLine();
+
+//     if(readResult != null){
+//        valueEntered = readResult;
+    
+//     } 
+    
+//     validNubmer = int.TryParse(valueEntered, out numValue);
+
+//     if (validNubmer == true){
+//         if(numValue <= 5 || numValue >= 10){
+//             validNubmer = false;
+//             Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
+//         }
+//     }else{
+//         Console.WriteLine("Sorry, you entered an invalid number, please try again");
+//     }
+
+// } while(validNubmer == false);
+
+// Console.WriteLine($"Your input value ({numValue}) has been accepted.");
+
+// readResult = Console.ReadLine();
+
+
+/*======================================================*/
+/*======================================================*/
+/*======================================================*/
+/*VALIDATE STRING INPUT*/
+
+// string? readResult;
+// string roleName = "";
+// bool validEntry = false;
+
+// Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+
+// do{
+//     readResult = Console.ReadLine();
+//     string formattedResult = "";
+
+//     if(readResult != null)
+//     {
+//         roleName = readResult;
+//     }
+
+//     formattedResult = roleName.Trim().ToLower();
+//     Console.WriteLine($"You input ({readResult}) was formatted to ({formattedResult})");
+
+//     if((formattedResult == "administrator") || (formattedResult == "manager") || (formattedResult == "user"))
+//     {
+//         validEntry = true;
+//     }
+//     else
+//     {
+//         Console.WriteLine($"The role name that you entered, \"{readResult}\", is not valid. Enter your role name (Administrator, Manager, or User)");
+//     }
+
+// }while(validEntry == false);
+
+// Console.WriteLine($"Your input value ({readResult}) has been accepted.");
+// readResult = Console.ReadLine();
+
+
+
+/*======================================================*/
+/*======================================================*/
+/*======================================================*/
+/*PROCESS THE CONTENTS OF A STRING ARRAY*/
+
+// string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+
+// int periodLocation;
+// string myString = "";
+
+// for(int i = 0; i < myStrings.Length; i++){
+
+//     myString = myStrings[i];
+//     periodLocation = myString.IndexOf(".");
+//     string mySentence;
+
+//     while(periodLocation != -1){
+
+//         // first sentence is the string value to the left of the period location
+//         mySentence = myString.Remove(periodLocation);
+
+//         // the remainder of myString is the string value to the right of the location
+//         myString = myString.Substring(periodLocation + 1);
+
+//         // remove any leading white-space from myString
+//         myString = myString.TrimStart();
+
+//         // update the comma location and increment the counter
+//         periodLocation = myString.IndexOf(".");
+
+//         Console.WriteLine(mySentence);
+//     }
+
+//     mySentence = myString.Trim();
+//     Console.WriteLine(mySentence);
+// }
+
+
+
+/*======================================================*/
+/*======================================================*/
+/*======================================================*/
+/*LANGUAGE INTEGRATED QUERY(LINQ)*/
+
+// List<int> scores = [100, 30,80,90,99,70,150];
+
+// IEnumerable<int> scoreQuery = 
+//     from score in scores 
+//     where score > 80
+//     orderby score descending
+//     select score;
+
+// List<int> myScores = scoreQuery.ToList();
+
+// foreach (var score in myScores){
+//     Console.WriteLine(score);
+// }
---
+++ test.cs
@@ -0,0 +1,269 @@
+// Console.WriteLine("Signed integral types:");
+
+// Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+// Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+// Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+// Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+// Console.WriteLine("");
+// Console.WriteLine("Unsigned integral types:");
+
+// Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+// Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+// Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+// Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+// Console.WriteLine("");
+// Console.WriteLine("Floating point types:");
+// Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+// Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+// Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+
+
+// int first = 2;
+// string second = "4";
+// int result = first + second;
+// Console.WriteLine(result);
+
+// int myInt = 3;
+// Console.WriteLine($"int: {myInt}");
+
+// decimal myDecimal = myInt;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+// string first = "5";
+// string second = "6";
+// int result = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(result);
+
+// using System.Globalization;
+// CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+// string value1 = "6";
+// string value2 = "5";
+// int result = Convert.ToInt32(value1) + Convert.ToInt32(value2);
+// Console.WriteLine(result);
+
+// int value = (int) 1.5m;
+// Console.WriteLine(value);
+
+// int value2 = Convert.ToInt32(1.5m);
+// Console.WriteLine(value2);
+
+
+// string[] values = {"ABC", "2", "90", "DEF", "5"};
+// string message = "";
+// int total = 0;
+
+// for(int i = 0; i < values.Length; i++)
+// {
+//     int number = 0;
+//     if(int.TryParse(values[i], out number))
+//     {
+//         total += number;
+//         // Console.WriteLine($"Total: {total}");
+//     }
+//     else
+//     {
+//         message += values[i];
+//         // Console.WriteLine($"Message: {message}");
+//     }
+
+// }
+
+// Console.WriteLine($"Total: {total}");
+// Console.WriteLine($"Message: {message}");
+
+// int value1 = 11;
+// decimal value2 = 6.2m;
+// float value3 = 4.3f;
+
+// // Your code here to set result1
+// // Hint: You need to round the result to nearest integer (don't just truncate)
+// int result1 = Convert.ToInt32 (value1/ value2);
+// Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+// // Your code here to set result2
+// decimal result2 = value2 / (decimal)value3;
+// Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// // Your code here to set result3
+// float result3 = value3 / value1;
+// Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+
+//#################################################################################
+//#################################################################################
+//#################################################################################
+
+//ARRAY OPERATIONS
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.Marshalling;
+
+string [] pallets = {"B14", "A11", "B12", "A13"};
+
+//sorting
+// Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+
+// foreach( var pallet in pallets)
+// {
+//     Console.WriteLine($"--{pallet}");
+// }
+
+// //reverse
+// Console.WriteLine("");
+// Console.WriteLine("Reversed...");
+// Array.Reverse(pallets);
+
+// foreach( var pallet in pallets)
+// {
+//     Console.WriteLine($"--{pallet}");
+// }
+
+//clear
+// Console.WriteLine("");
+
+// // Console.WriteLine($"Before: {pallets[0].ToLower()}");
+// Array.Clear(pallets, 0, 2);
+
+// // if (pallets[0] != null)
+// // {
+// //     Console.WriteLine($"After: {pallets[0].ToLower()}");
+// // }
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"--{pallet}");
+// }
+
+// //resize
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine($"Resizing to 6...count: {pallets.Length}");
+
+// pallets[4] = "MN32";
+// pallets[5] = "AZ40";
+
+// foreach( var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 5);
+// Console.WriteLine($"Resizing to 5...count: {pallets.Length}");
+// foreach( var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+
+
+
+//Split() and Join()
+
+// string value = "abc123";
+// char[] valueArray = value.ToCharArray();
+// //Console.WriteLine(valueArray);
+// Array.Reverse(valueArray);
+// //Console.WriteLine(valueArray);
+// //string result = new string(valueArray);
+
+// string result = String.Join(",", valueArray); //convert character array to comma-separated string array 
+// Console.WriteLine(result);
+
+// //split 
+// string[] items = result.Split(",");
+// foreach (string item in items)
+// {
+//     Console.WriteLine(item);
+// }
+
+
+
+
+//#################################################################################
+//#################################################################################
+//#################################################################################
+
+//CODING CHALLENGE: REVERSE THE WORDS IN A SENTENCE
+// string pangram = "The quick brown fox jumps over the lazy dog";
+
+// string[] itemsArray = pangram.Split(" ");
+
+// // foreach (string item in itemsArray)
+// // {
+// //     Console.WriteLine(item);
+// // }
+
+// // create an empty array to be used to store final result
+// string [] newItemsArray = new string[itemsArray.Length];
+// // Console.WriteLine(newItemsArray);
+
+// //Iterate through each item to reverse the characters
+// for (int i = 0; i < itemsArray.Length; i++)
+// {
+//     char[] letters = itemsArray[i].ToCharArray();
+//     Array.Reverse(letters);
+//     // Console.WriteLine(letters);
+
+//     // put the reversed items into the empty array
+//     newItemsArray[i] = new string(letters);
+// }
+
+// // display result
+// string result =  String.Join(" ", newItemsArray);
+// Console.WriteLine(result);
+
+
+
+//#################################################################################
+//#################################################################################
+//#################################################################################
+
+//CODING CHALLENGE: SORD ORDER IDS AND TAG ERRORS
+
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+//split string and put elements in an array
+string[] orderArray = orderStream.Split(",");
+
+// sort array alphabetically
+Array.Sort(orderArray); 
+// foreach(string item in orderArray)
+// {
+//     Console.WriteLine(item);
+// }
+
+for(int i = 0; i < orderArray.Length; i++)
+{
+    char[] id = orderArray[i].ToCharArray();
+
+    if(id.Length != 4)
+    {
+        Console.WriteLine($"{orderArray[i]}     - Error");
+    }
+    else
+    {
+        Console.WriteLine(orderArray[i]);
+    }
+}
+
+
+// OR
+
+// foreach (var item in items)
+// {
+//     if (item.Length == 4)
+//     {
+//         Console.WriteLine(item);
+//     }
+//     else
+//     {
+//         Console.WriteLine(item + "\t- Error");
+//     }
+// }
